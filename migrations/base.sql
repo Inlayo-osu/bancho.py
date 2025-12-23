d5fb5c61cf5eabcf63487bcded686d077a980728
Inlayo <Inlayo@Inlayo.com>
2025-12-24 05:04:22 +0900

    fix: update BanchoBot user details for correct email and country

--- migrations/base.sql
+++ migrations/base.sql
@@ -373,8 +373,8 @@ create index users_country_index
 	on users (country);
 
 insert into users (id, name, safe_name, priv, country, silence_end, email, pw_bcrypt, creation_time, latest_activity)
-values (1, 'BanchoBot', 'banchobot', 1, 'ca', 0, 'bot@akatsuki.pw',
-        '_______________________my_cool_bcrypt_______________________', UNIX_TIMESTAMP(), UNIX_TIMESTAMP());
+values (1, 'BanchoBot', 'banchobot', 1, 'kr', 0, 'bot@inlayo.com',
+        'banchobot', UNIX_TIMESTAMP(), UNIX_TIMESTAMP());
 
 INSERT INTO stats (id, mode) VALUES (1, 0); # vn!std
 INSERT INTO stats (id, mode) VALUES (1, 1); # vn!taiko
